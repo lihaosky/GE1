42b77b0f467db06000f589c3d4085ecfeefee04b
Hao Li <lihaosky@hotmail.com>
2011-11-18 15:44:33 -0500

    Deleted unneeded import

--- src/client/ClientImp.java
+++ src/client/ClientImp.java
@@ -1,12 +1,7 @@
 package client;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.rmi.RemoteException;
