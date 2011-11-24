145c1a208eedd76fbf794545f5cd6b650d2e4669
Hao Li <lihaosky@hotmail.com>
2011-11-24 12:38:24 -0500

    Read slave ip from config file

--- src/master/NodeManager.java
+++ src/master/NodeManager.java
@@ -1,5 +1,9 @@
 package master;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -15,7 +19,26 @@ public class NodeManager {
 	 * Find node list
 	 */
 	public static void findNodes() {
-		
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(Parameters.nodeListFile));
+			String line = br.readLine();
+			while (line != null) {
+				nodeList.add(new Node(line, Node.AVAILABLE));
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Can't find file " + Parameters.nodeListFile);
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Read file error!");
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	/**
@@ -23,15 +46,25 @@ public class NodeManager {
 	 * @param n number of nodes to get
 	 */
 	synchronized public static ArrayList<Node> getNodes(int n) {
+		int addedNum = 0;
 		ArrayList<Node> newList = new ArrayList<Node>();
-		Node node = new Node("locahost", Node.AVAILABLE);
-		if (node.connect() == null) {
-			System.out.println("Find slave error!");
-			node.setStatus(Node.DEAD);
-			return null;
+		for (int i = 0; i < nodeList.size(); i++) {
+			Node node = nodeList.get(i);
+			if (node.getStatus() == Node.AVAILABLE) {
+				//Try to connect to the node
+				if (node.connect() == null) {
+					System.out.println("Find slave error!");
+					node.setStatus(Node.DEAD);
+				} else {
+					node.setStatus(Node.BUSY);
+					newList.add(node);
+					addedNum++;
+					if (addedNum == n) {
+						break;
+					}
+				}
+			}
 		}
-		node.setStatus(Node.BUSY);
-		newList.add(node);
 		return newList;
 	}
 	
