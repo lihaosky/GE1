1a80e03978feb691c23147d66fafb17ea8825bcb
Hao Li <lihaosky@hotmail.com>
2011-11-25 23:49:51 -0500

    Compiled classes

--- bin/client.sh
+++ bin/client.sh
@@ -1,1 +1,1 @@
-java -Djava.security.policy=java.policy client.Client -D/home/lihao/Desktop/GE_Project/fourpool-cornell -O/home/lihao/Desktop/GE_Project/ 10 10
+java -Djava.security.policy=java.policy client.Client -D/home/lihao/Desktop/GE_Project/fourpool-cornell -O/home/lihao/Desktop/GE_Project/ 1  10
