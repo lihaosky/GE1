72fbf914b271f8fbcaa421ca485f6d297516c37c
Hao Li <lihaosky@hotmail.com>
2011-11-28 00:41:08 -0500

    changed client.sh

--- bin/client.sh
+++ bin/client.sh
@@ -1,1 +1,1 @@
-java -Djava.security.policy=java.policy client.Client -D/home/lihao/Desktop/GE_Project/fourpool-cornell -O/home/lihao/Desktop/GE_Project/ 8 4 
+java -Djava.security.policy=java.policy client.Client -D/home/lihao/Desktop/GE_Project/fourpool-cornell -O/home/lihao/Desktop/GE_Project/ 8 1 
